42c517d6b8f7b1bd94d18678375d8914f4fbaff2
Anderson <faro.anderson@gmail.com>
2020-09-10 10:19:44 -0300

    mudanças adicionadas

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,10 +1,9 @@
-import React, { useState, FormEvent } from "react";
-
-import { Form, Films, Error } from './styles';
+import React, { useState, FormEvent, useEffect } from 'react';
 
 import { FiChevronRight } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
-import api from '../../services/api'
+import { Form, Films, Error } from './styles';
+import api from '../../services/api';
 import Header from '../../components/Header/styles';
 
 interface IProperties {
@@ -13,17 +12,16 @@ interface IProperties {
   adult: false;
   release_date: string;
   original_title: string;
-  overview:string;
-  backdrops:string;
-
+  overview: string;
+  backdrops: string;
 }
 const Dashboard: React.FC = () => {
-
   const [newSearch, setNewSearch] = useState('');
 
   const [inputError, setInputError] = useState('');
 
   const [movies, setMovies] = useState<IProperties[]>([]);
+  const [page, total_results, total_pages] = useState();
 
   async function searhMovie(event: FormEvent<HTMLFormElement>): Promise<void> {
     event.preventDefault();
@@ -33,68 +31,56 @@ const Dashboard: React.FC = () => {
       return;
     }
 
-
     try {
       const api_url = '2bf45dbd029ec4fbc6d4df66adb594c9';
 
-      const response = await api.get(`/search/movie?api_key=${api_url}&query= ${newSearch}`);
+      const response = await api.get(
+        `movie/popular?api_key=${api_url}&page=1`,
+        // `/search/movie?api_key=${api_url}&query= ${newSearch}`,
+      );
 
-      const results = response.data.results;
+      const { results } = response.data;
 
-        setMovies(results);
-        setNewSearch('');
-        setInputError('');
-        console.log(results);
-
-    }
-
-    catch {
+      setMovies(results);
+      setNewSearch('');
+      setInputError('');
+      console.log(results);
+    } catch {
       setInputError('Tente novamente');
     }
-
   }
-
 
   return (
     <>
       <Header>Movies</Header>
-
-      <Form hasError={!!inputError} onSubmit={searhMovie} >
-
+      <Form hasError={!!inputError} onSubmit={searhMovie}>
         <input
-
           value={newSearch}
-          onChange={(e) => setNewSearch(e.target.value)}
+          onChange={e => setNewSearch(e.target.value)}
           placeholder="
            Busque um filme por nome ano ou gênero..."
         />
-
       </Form>
-
       {inputError && <Error>{inputError}</Error>}
-
-
       <Films>
-        {movies.map((movie, mv ) => (
-          <Link key={mv}
-            to={"" }>
+        {movies.map((movie, mv) => (
+          <Link key={mv} to={''}>
             <img
               src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
               alt={movie.poster_path}
             />
             <div>
-              <p className= "original_title">{movie.original_title}</p>
+              <p className="original_title">{movie.original_title}</p>
               <p>{movie.release_date}</p>
               <p>{movie.overview}</p>
             </div>
             <FiChevronRight size={20} />
           </Link>
-
         ))}
-
       </Films>
+      <Pagination> </Pagination>
     </>
-  )
-}
+  );
+};
 
 export default Dashboard;
