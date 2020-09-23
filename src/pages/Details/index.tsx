e82029c4c3a079873d7747622e66f54e1a7f7066
Anderson <faro.anderson@gmail.com>
2020-09-23 08:48:03 -0300

    details com mudança

--- src/pages/Details/index.tsx
+++ src/pages/Details/index.tsx
@@ -1,12 +1,11 @@
-import React, { useState, FormEvent, useEffect } from 'react';
-import { useRouteMatch, Link } from 'react-router-dom';
-
-import { FiChevronRight } from 'react-icons/fi';
+import React, { useState, useEffect, useCallback } from 'react';
+import { useRouteMatch } from 'react-router-dom';
 
 import api from '../../services/api';
 
 interface IFilmes {
   poster_path: string;
+
   adult: false;
   release_date: string;
   original_title: string;
@@ -17,16 +16,27 @@ interface DetailsParams {
   details: string;
 }
 
-const Details: React.FC = () => {
+const Details: React.FC = movieId => {
   const [films, setFilms] = useState<IFilmes | null>(null);
 
   const { params } = useRouteMatch<DetailsParams>();
 
+  const fetchDate = useCallback(async () => {
+    try {
+      const api_url = '2bf45dbd029ec4fbc6d4df66adb594c9';
+
+      api.get(`/movie/${movieId}?api_key = ${api_url}`).then(response => {
+        const { results } = response.data;
+        console.log(results);
+      });
+    } catch {
+      console.log('error');
+    }
+  }, [movieId]);
+
   useEffect(() => {
-    api.get(`search/movie${params.details}`).then(response => {
-      setFilms(response.data);
-    });
-  }, [params.details]);
+    fetchDate();
+  });
 
   return (
     <>
